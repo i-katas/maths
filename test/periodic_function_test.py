82134cff0b75d330d61f9b9f04c0885b16933926
i-katas <feexonj@gmail.com>
2021-05-27 11:53:30 +0800

    inline start parameter

--- test/periodic_function_test.py
+++ test/periodic_function_test.py
@@ -1,12 +1,8 @@
 def test_period():
-    assert f(1) == 0
-    assert f(7) == 6
-    assert f(8) == 0
-
-    assert f(1, 1) == 1
-    assert f(7, 1) == 7
-    assert f(8, 1) == 1
+    assert f(1) == 1
+    assert f(7) == 7
+    assert f(8) == 1
 
 
-def f(x, start=0):
-    return (x - 1) % 7 + start
+def f(x):
+    return (x - 1) % 7 + 1
