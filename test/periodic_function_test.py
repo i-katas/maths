c4b854ad2264ccadba339ec0ff6c89c4012a6d84
i-katas <feexonj@gmail.com>
2021-05-27 11:51:28 +0800

    shift independent variable right one unit

--- test/periodic_function_test.py
+++ test/periodic_function_test.py
@@ -1,12 +1,12 @@
 def test_period():
-    assert f(0) == 0
-    assert f(6) == 6
-    assert f(7) == 0
+    assert f(1) == 0
+    assert f(7) == 6
+    assert f(8) == 0
 
-    assert f(0, start = 1) == 1
-    assert f(6, 1) == 7
-    assert f(7, 1) == 1
+    assert f(1, 1) == 1
+    assert f(7, 1) == 7
+    assert f(8, 1) == 1
 
 
 def f(x, start=0):
-    return x % 7 + start
+    return (x - 1) % 7 + start
