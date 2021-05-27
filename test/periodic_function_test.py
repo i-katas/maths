6471b9d06bca5f02cf27c68130f63b6a794990af
i-katas <feexonj@gmail.com>
2021-05-27 11:47:11 +0800

    change starts value

--- test/periodic_function_test.py
+++ test/periodic_function_test.py
@@ -3,6 +3,10 @@ def test_period():
     assert f(6) == 6
     assert f(7) == 0
 
+    assert f(0, start = 1) == 1
+    assert f(6, 1) == 7
+    assert f(7, 1) == 1
 
-def f(x):
-    return x % 7
+
+def f(x, start=0):
+    return x % 7 + start
